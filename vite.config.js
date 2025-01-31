1cb3e6d316bd647be8df503194cc743b3f1e61a9
bhagchand-dawla <bhagchanddawla@gmail.com>
2025-02-01 04:52:06 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+   server: {
+    host: '0.0.0.0',  // Ensures it binds to all network interfaces
+    port: 5173,       // Port for the dev server, ensure it's consistent with your Render setup
+  },
 });
